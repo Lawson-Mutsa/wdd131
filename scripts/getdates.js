b98e2b3ca039576aab7ef2c46dcda620b4406362
Lawson-Mutsa <admin@lawsonmutsa.com>
2025-03-11 08:58:20 +0200

    Modified the HTML, CSS and Javascript Files

---
+++ scripts/getdates.js
@@ -0,0 +1,7 @@
+// Get the current year
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = currentYear;
+
+// Get the last modified date of the document
+const lastModifiedDate = document.lastModified;
+document.getElementById('lastModified').textContent = lastModifiedDate;
